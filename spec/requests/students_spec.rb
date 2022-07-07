8e9680a2f9aa08be465ea263b8ca48126b7c22f0
Nishant <nishantnagdeve@gmail.com>
2022-07-07 15:00:56 +0530

    test using rspec

---
+++ spec/requests/students_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Students Api', type: :request do
+  it 'returns all books' do
+	get '/api/v1/students'
+
+	expect(response).to have_http_status(:success)
+	expect(JSON.parse(response.body).size).to eq(0)
+  end
+end
