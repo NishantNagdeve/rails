0d910ded2c715b0fc80031f84a79ff88422aa091
Nishant <nishantnagdeve@gmail.com>
2022-07-07 15:08:56 +0530

    update rspec

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Students Api', type: :request do
-  it 'returns all books' do
+  it 'returns all students' do
 	get '/api/v1/students'
 
 	expect(response).to have_http_status(:success)
